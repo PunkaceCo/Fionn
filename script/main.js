586ce3788b9c7052535265f95cd6665e87849018
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-12-03 22:26:13 +0330

    update

--- script/main.js
+++ script/main.js
@@ -23,13 +23,13 @@ window.onclick = function(event) {
 
 function myFunction() {
   var x = document.getElementById("mob");
-  var icon = document.getElementById("icon")
+  var icon = document.getElementById("icon");
   if (x.style.display === "block") {
     x.style.display = "none";
-    icon.style.display = "block"
+    icon.style.display = "block";
   } else {
     x.style.display = "block";
-    icon.style.display = "block"
+    icon.style.display = "block";
 
   }
 }
--- script/performance.js
+++ script/performance.js
@@ -1,17 +1,28 @@
-var mobileMenu = document.getElementById("mob");
-var iconMenu = document.getElementById("icon")
+// var mobileMenu = document.getElementById("mob");
+// var iconMenu = document.getElementById("icon")
 
-var gridContainer = document.getElementById('maingrid');
+// var gridContainer = document.getElementById('maingrid');
 
+
+// function myFunction() {
+
+//   if (mobileMenu.style.display === "block") {
+//     mobileMenu.style.display = "none";
+//     iconMenu.style.display = "block"
+//   } else {
+//     mobileMenu.style.display = "block";
+//     iconMenu.style.display = "block"
+
+//   }
+// }
+var x = document.getElementById("myLinks");
 
 function myFunction() {
-
-  if (mobileMenu.style.display === "block") {
-    mobileMenu.style.display = "none";
-    iconMenu.style.display = "block"
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
   } else {
-    mobileMenu.style.display = "block";
-    iconMenu.style.display = "block"
-
+    x.style.display = "block";
   }
 }
+
