68443a9562e2a3ef91379ae25667ebd7a2167885
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-12-06 16:40:01 +0330

    slider done

---
+++ script/gallery.js
@@ -0,0 +1,66 @@
+const slideContainer = document.querySelector('.slider');
+
+const sliderText = document.querySelector('.slider--text');
+const sliderTitle = document.querySelector('.slider--title');
+
+
+const btnLeft = document.querySelector('.slider__btn-left');
+const btnRight = document.querySelector('.slider__btn-right');
+
+const sliderImages = [
+{src:'../asset/png/pic02.png'},
+{src:'../asset/png/pic03.png'},
+{src:'../asset/png/pic04.png'},
+{src:'../asset/png/pic05.png'},
+{src:'../asset/png/pic06.png'},
+{src:'../asset/png/pic07.png'},
+];
+
+
+let slideCounter = 0;
+
+const startSlider = () => {
+  slideContainer.style.backgroundImage = ` url(${sliderImages[0].src})`;
+};
+
+btnRight.addEventListener('click', function() {
+  if (slideCounter === sliderImages.length - 1) {
+    slideContainer.style.backgroundImage = `url(${sliderImages[0].src})`;
+    slideCounter = -1;
+
+    slideContainer.classList.add('fadeIn');
+    setTimeout(() => {
+      slideContainer.classList.remove('fadeIn');
+    }, 1000);
+  }
+  slideContainer.style.backgroundImage = `url(${sliderImages[slideCounter + 1].src})`;
+
+
+  slideCounter++;
+  slideContainer.classList.add('fadeIn');
+  setTimeout(() => {
+    slideContainer.classList.remove('fadeIn');
+  }, 1000);
+});
+
+btnLeft.addEventListener('click', function() {
+  if (slideCounter === 0) {
+    slideContainer.style.backgroundImage = `url(${sliderImages[sliderImages.length - 1].src})`;
+
+    slideCounter = sliderImages.length;
+    slideContainer.classList.add('fadeIn');
+    setTimeout(() => {
+      slideContainer.classList.remove('fadeIn');
+    }, 1000);
+  }
+
+  slideContainer.style.backgroundImage = `url(${sliderImages[slideCounter - 1].src})`;
+
+  slideCounter--;
+  slideContainer.classList.add('fadeIn');
+  setTimeout(() => {
+    slideContainer.classList.remove('fadeIn');
+  }, 1000);
+});
+
+document.addEventListener('DOMContentLoaded', startSlider);
