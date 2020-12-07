994e8392b782ce628cfc829f6c4c33f6812bc08d
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-12-07 20:15:34 +0330

    landing update

--- script/landing.js
+++ script/landing.js
@@ -1,15 +1,17 @@
-var background = document.body.background;
-console.log("bg",background);
+document.body.style.background="#52575D url(../asset/svg/landing/main.png) no-repeat center bottom";
+
 function mouseEnter(e) {
   console.log("e",e.id);
   if(e.id == "landing-right"){
-    document.body.style.background ="#52575D url(../asset/png/LNDASS0021.png) no-repeat fixed bottom ";
+    document.body.style.background ="#52575D url(../asset/svg/landing/right.png) no-repeat center bottom fixed ";
+    document.body.style.transition = 'background-image 1s';
   }
   
   // e.style.opacity = "10"
   // background.style.opacity = "0"
   if(e.id == "landing-left"){
-    document.body.style.background ="#595B83 url(../asset/svg/LNDASSgg.svg) no-repeat fixed bottom";
+    document.body.style.background ="#595B83 url(../asset/svg/landing/left.png) no-repeat center bottom fixed";
+    document.body.style.transition = 'background-image 1s';
   }
   
   e.style.opacity="10";
@@ -17,9 +19,9 @@ function mouseEnter(e) {
 
 function mouseLeave(e) {
   console.log("e",e.id);
-  document.body.style.background ="#52575D url(../asset/svg/LNDA.svg) no-repeat fixed bottom";
+  document.body.style.background ="#52575D url(../asset/svg/landing/main.png) no-repeat center bottom fixed";
+  document.body.style.transition = 'background-image 1s';
   e.style.opacity ="0";
-
 }
 
 
