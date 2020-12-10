77a68a118d72cc48078a2551798cd0fc3010a941
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-12-10 19:37:22 +0330

    update

---
+++ script/profile.js
@@ -0,0 +1,8 @@
+var p = document.getElementById('about');
+// var des = document.querySelector("#aboutme");
+console.log(p);//Defines the function to be called on click
+
+
+p.onclick = function(e){
+  console.log("hi");
+}
