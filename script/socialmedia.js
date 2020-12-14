7f016345a7cc292eeabf50c82c6764554a66db73
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-12-14 18:56:22 +0330

    font size update

--- script/socialmedia.js
+++ script/socialmedia.js
@@ -1,17 +1,37 @@
 let icons = [
-  {icon:'../'},
-  {icon:'../'},
-  {icon:'../'},
-  {icon:'../'},
+  {
+    icon:'../',
+    address:''
+  },
+  {
+    icon:'../',
+    address:''
+  },
+  {
+    icon:'../',
+    address:''
+  },
+  {
+    icon:'../',
+    address:''
+  },
 ];
-
+var links = document.querySelector('.component-links');
+var newLink = document.createElement('a');
 var newIcon = document.createElement('i');
-function loadIcons(n){
+newLink.appendChild(newIcon);
 
+
+function loadIcons(n){
+  
 }
 
 function addIcons(){
-
+  for (var i = 0; i < information.length; i++) {
+    newLink.addEventListener('DOMContentLoaded', loadIcons(i));
+    var clone = newLink.cloneNode(true);
+    links.appendChild(clone);
+  }
 }
 
 addIcons();
