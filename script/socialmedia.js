eaefe177ab090a8ace9414ac5ef020c6d8cbfd19
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-12-14 16:14:48 +0330

    update

---
+++ script/socialmedia.js
@@ -0,0 +1,17 @@
+let icons = [
+  {icon:'../'},
+  {icon:'../'},
+  {icon:'../'},
+  {icon:'../'},
+];
+
+var newIcon = document.createElement('i');
+function loadIcons(n){
+
+}
+
+function addIcons(){
+
+}
+
+addIcons();
