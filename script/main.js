31efc3365a37bc1172ec72284f80ec68311f5c54
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-12-15 18:23:06 +0330

    social media icon update

--- script/main.js
+++ script/main.js
@@ -5,11 +5,11 @@ var menu = document.getElementById('menu');
 var icon = document.getElementById('icon');
 var close = document.getElementById('close');
 var logo = document.getElementById('logo');
-
+console.log(content);
   if(menu.style.display === 'block'){
     icon.style.display = 'block';
     menu.style.display = 'none';
-    content.style.display = 'none'; 
+    content.style.display = 'none';
     // close.style.display = 'none';
     logo.style.display = 'none';
 
@@ -20,6 +20,8 @@ var logo = document.getElementById('logo');
     menu.style.display = 'block';
     content.style.display = 'block';
     logo.style.display = 'block';
+    content.style.backgroundColor= '#ff9b045b' ;
+
 
   }
 }
