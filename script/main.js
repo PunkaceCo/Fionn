d9fa3fbb26ff33df56fd9482efa5099543415e90
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-12-22 14:10:25 +0330

    mobile menu icon update

--- script/main.js
+++ script/main.js
@@ -6,12 +6,14 @@ var icon = document.getElementById('icon');
 var close = document.getElementById('close');
 var logo = document.getElementById('logo');
 console.log(menu);
+
   if(menu.style.display === 'block'){
     icon.style.display = 'block';
     menu.style.display = 'none';
     content.style.display = 'none';
     // close.style.display = 'none';
     logo.style.display = 'none';
+    // logo.style.borderRadius = " 0 0 25px 0";
 
   }
   else{
@@ -24,10 +26,10 @@ console.log(menu);
     // menu.style.backgroundColor= '#ff9b045b' ;
   }
 }
-// var item = document.getElementById('item3');
+var item = document.getElementById('main');
 
-// window.onclick = function(event) {
-//   if (event.target == item) {
-//     menu.style.display = "none";
-//   }
-// };
+window.onclick = function(event) {
+  if (event.target == item) {
+    menu.style.display = "none";
+  }
+};
